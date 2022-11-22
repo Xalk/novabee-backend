5139533a0e9bf0c6efc2d4504a1268d228f403ec
Artem Khalturin <goxalk@gmail.com>
2022-11-22 19:06:00 +0200

    add checking if uploads directory exist

--- config/uploadStorage.ts
+++ config/uploadStorage.ts
@@ -1,7 +1,11 @@
 import multer from "multer";
+import fs from 'fs';
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+        if(!fs.existsSync('uploads')){
+            fs.mkdirSync('uploads')
+        }
         cb(null, 'uploads')
     },
     filename: (_, file, cb) => {
