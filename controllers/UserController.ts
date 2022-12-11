886a968a267104bf7f85b0b1b930aa68a8fea84a
Artem Khalturin <goxalk@gmail.com>
2022-12-11 14:59:31 +0200

    fix login response body

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -74,7 +74,7 @@ export const login = async (req: Request, res: Response) => {
 
         const {passwordHash, ...userData} = user._doc as IUser;
 
-        res.json({...userData, token});
+        res.json({userData, token});
     } catch (error) {
         console.log(error);
         res.status(500).json({
