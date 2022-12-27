f994f29b27da1ee7c5674a6ec58ebd27875de006
Artem Khalturin <goxalk@gmail.com>
2022-12-27 20:49:06 +0200

    change queen

--- models/Queen.ts
+++ models/Queen.ts
@@ -1,37 +1,36 @@
 import mongoose from 'mongoose';
-import {IQueen} from "../config/interface";
+import { IQueen } from '../config/interface';
 
 const QueenSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-        description: {
-            type: String,
-            trim: true,
-        },
-        introducedFrom: {
-            type: Date,
-            required: true,
-        },
-        reQueenedFrom: {
-            type: Date,
-        },
-        isOut: {
-            type: Boolean,
-            default: false
-        },
-        beehive: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Beehive',
-            required: true,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
+    description: {
+      type: String,
+      trim: true,
     },
+    introducedFrom: {
+      type: Date,
+      required: true,
+    },
+    reQueenedFrom: {
+      type: Date,
+    },
+    isOut: {
+      type: Boolean,
+    },
+    beehive: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Beehive',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 export default mongoose.model<IQueen>('Queen', QueenSchema);
--- routes/queenRouter.ts
+++ routes/queenRouter.ts
@@ -2,17 +2,16 @@ import express from "express";
 import * as QueenController from "../controllers/QueenController"
 import checkAuth from "../utils/checkAuth";
 import {canGetBeehive} from "../utils/canGetBeehive";
-import {queenCreateValidation} from "../validations/queen";
-import handleValidationErrors from "../utils/handleValidationErrors";
+
 
 const router = express.Router({mergeParams: true})
 
 
-router.post('/queen', checkAuth, canGetBeehive, queenCreateValidation, handleValidationErrors, QueenController.create)
+router.post('/queen', checkAuth, canGetBeehive, QueenController.create)
 
 router.get('/queen', checkAuth, canGetBeehive,  QueenController.getOne)
 
-router.patch('/queen', checkAuth, canGetBeehive, queenCreateValidation, handleValidationErrors, QueenController.update)
+router.patch('/queen', checkAuth, canGetBeehive, QueenController.update)
 
 router.delete('/queen', checkAuth, canGetBeehive, QueenController.remove)
 
