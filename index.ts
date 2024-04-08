d3d2eba47c6f9faa2b255a95514773ccf8a2e540
Xalk <39776719+Xalk@users.noreply.github.com>
2024-04-08 17:07:26 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 // Middleware
 const app = express();
 app.use(express.json());
-app.use(cors('*'));
+app.use(cors({origin: '*'}));
 app.use('/uploads', express.static('uploads'));
 
 // Routes
